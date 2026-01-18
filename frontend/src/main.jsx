6fe7ff2571d85253847ee0e9d040df85167b722e
Priyanka Sonawane <priyankasonawane7666@gmail.com>
2026-01-18 18:41:15 +0530

    Fix routing using HashRouter for deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+<BrowserRouter>
+  <App />
+</BrowserRouter>
+
 );
